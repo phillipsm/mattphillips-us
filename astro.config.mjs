028cafc3f1c73de684bc951cb669e80d2bacdfa5
Matt Phillips <matt@mooncreativelab.com>
2026-05-23 22:32:04 -0500

    optimized images for faster download and added sitemap, schema for the person (me, matt phillips), ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mattphillips.us',
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
